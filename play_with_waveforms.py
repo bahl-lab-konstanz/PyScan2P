c3215c6056e46b928d935db7c89a50573d35501f
arminbahl <arbahl@gmail.com>
2022-07-09 13:31:45 +0200

    Simple version of PyZebra2P

--- play_with_waveforms.py
+++ play_with_waveforms.py
@@ -1,74 +1,49 @@
 import numpy as np
-import numpy as np
 import matplotlib
-matplotlib.use("MacOSX")
+matplotlib.use("QT5AGG")
 import pylab as pl
-from scipy.ndimage import gaussian_filter1d, convolve1d
-from numba import jit
+from scipy.ndimage import gaussian_filter1d
 
-@jit(nopython=True)
-def do_binning(img_channel0, img_channel1, pmt_data_channel0, pmt_data_channel1, x, y, bin_size):
+x_pixels = 512
+y_pixels = 512
+turnaround_pixels = 20
+pixel_to_galvo_factor = 0.006
 
-    for i in range(len(x)):
-        for j in range(bin_size):
-            img_channel0[round(y[i]), round(x[i])] += pmt_data_channel0[i * bin_size + j] / bin_size
-            img_channel1[round(y[i]), round(x[i])] += pmt_data_channel1[i * bin_size + j] / bin_size
-
-
-x_pixels = 500
-y_pixels = 500
-
-x_line = np.arange(0, x_pixels, 1.0)
+# Create the waveform
+x_line = np.arange(0, x_pixels + turnaround_pixels*2, 1.0)
 
 x = []
 y = []
 for i in range(y_pixels):
-    if i%2 == 0:
+    if i % 2 == 0:
         x.extend(x_line)
     else:
         x.extend(x_line[::-1])
-    y.extend(np.ones(x_pixels, dtype=np.float)*i)
+    y.extend(np.ones(x_pixels + turnaround_pixels*2, dtype=np.float) * i)
 
-pl.plot(x, y)
+# Make all the dynamics slower, to help our poor galvos
 
-dx = np.diff(x)
-dy = np.diff(y)
+# Concatenate to also take care of the jumps between frames
+dx = np.diff(np.r_[x, x, x])
+dy = np.diff(np.r_[y, y, y])
 
+# low-pass filter the velocities
 dx = gaussian_filter1d(dx, sigma=5)
 dy = gaussian_filter1d(dy, sigma=5)
 
-x = np.cumsum(dx)
-y = np.cumsum(dy)
+# Undo the concatenation
+x = np.cumsum(dx)[len(x):len(x) * 2]
+y = np.cumsum(dy)[len(x):len(x) * 2]
 
-# x and y are the locations in your image!
-pixel_to_galvo_factor = 0.002
-vx = (x - np.mean(x))*pixel_to_galvo_factor
-vy = (y - np.mean(y))*pixel_to_galvo_factor
+# The voltage of the galvos is simply a facotor times the mean-centered pixel location
+Vx = (x - np.mean(x)) * pixel_to_galvo_factor
+Vy = (y - np.mean(y)) * pixel_to_galvo_factor
 
-# the input rate from the pmts, is higher!
-bin_size = 5
-output_rate = 500000
-input_rate = output_rate*bin_size # Ma x 3.57 MhZ allowed!!!
+# Round the pixel locations to be able to later use it as indices to place in the image
+x = np.round(x).astype(np.uint16)
+y = np.round(y).astype(np.uint16)
 
-
-# send this to galvos with a output rate
-
-
-pl.plot(vx, vy)
+pl.plot(Vx, Vy)
+pl.figure()
+pl.plot(x, y)
 pl.show()
-
-
-pmt_channel0 = np.random.random(size=len(x)*bin_size)*20000 + 10000
-pmt_channel1 = np.random.random(size=len(x)*bin_size)*20000 + 10000
-img_channel0 = np.zeros((x_pixels, y_pixels), dtype=np.float64)
-img_channel1 = np.zeros((x_pixels, y_pixels), dtype=np.float64)
-
-do_binning(img_channel0, img_channel1, pmt_channel0, pmt_channel1, x, y, bin_size)
-
-img_channel0 = img_channel0.astype(np.uint16)
-img_channel1 = img_channel1.astype(np.uint16)
-
-pl.imshow(img_channel0)
-pl.show()
-
-
